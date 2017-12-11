7d357bf40cf9d2e38cf9b5236660e35b22376670
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-12-11 10:15:10 +0800

    修复二维码无法保存本地bug

--- pages/qrcode/qrcode.js
+++ pages/qrcode/qrcode.js
@@ -5,7 +5,9 @@ Page({
 
   data: {
     content: "",
-    logo: ''
+    logo: '',
+    alipay: '',
+    wechat: ''
   },
 
   onLoad: function (options) {
@@ -17,18 +19,51 @@ Page({
     console.log(options)
     this.setData({
       content: 'https://heyfox.herokuapp.com/pay?ali=' + options['alipay'] + '&wx=' + options['wechat'],
+      alipay: options['alipay'],
+      wechat: options['wechat'],
       logo: options['logo']
     })
-    
+    console.log('-----------')
+    console.log(this.data['content'])
+    console.log(this.data['logo'])
+    console.log('-======---')
+
     // 绘图
     const ctx = wx.createCanvasContext('mycanvas')
     QR.qrApi.draw(this.data.content, ctx, 300, 300)
-    if (options['logo'] != '') {
-      console.log(options['logo'])
-      // ctx.clearRect(129, 129, 42, 42)
+    if (options['logo'] != '' & options['logo'] != null) {
+      console.log("绘制logo")
+      ctx.clearRect(129, 129, 42, 42)
       ctx.drawImage(options['logo'], 130, 130, 40, 40)
     }
     ctx.draw()
+  },
+
+  // 分享
+  onShareAppMessage: function (res) {
+    if (res.from === 'menu') {
+      // 右上角转发菜单
+      console.log(res.target)
+    }
+    return {
+      title: '合并收款码',
+      path: 'pages/qrcode/qrcode?alipay=' + this.data.alipay + "&wechat=" + this.data.wechat,
+      success: function (res) {
+        // 转发成功
+        console.log('转发成功')
+        console.log('pages/qrcode/qrcode?alipay=' + this.data.alipay + "&wechat=" + this.data.wechat)
+        wx.showToast({
+          title: '转发成功',
+        })
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log('转发失败')
+        wx.showToast({
+          title: '转发失败',
+        })
+      }
+    }
   },
 
   previewImg: function (e) {
@@ -43,18 +78,15 @@ Page({
             success: function (res) {
               var tempFilePath = encodeURI(res.tempFilePath)
               console.log(res)
-              wx.saveFile({
-                tempFilePath: res.tempFilePath,
+              wx.saveImageToPhotosAlbum({
+                filePath: res.tempFilePath,
                 success: function success(res) {
                   console.log('saved::' + res.savedFilePath);
                   wx.showToast({
                     title: '保存成功',
                   })
-                },
-                complete: function fail(e) {
-                  console.log(e.errMsg);
                 }
-              });
+              })
             },
             fail: function (res) {
               console.log(res);
